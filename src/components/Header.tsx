e5f34f8fc6d13be74a7d0f3ec72d5132be2a2b3e
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-03-31 22:21:12 +0700

    Create Header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import useMusicPlayer from "../hooks/useMusicPlayer";
+
+export default function Header() {
+  const { currentTrackName } = useMusicPlayer();
+  return (
+    <div className="header">
+      <p className="moving-text">{currentTrackName}</p>
+    </div>
+  );
+}
